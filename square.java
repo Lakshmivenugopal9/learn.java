f424308aa671aa88bf11ab447e970dacfa618843
Lakshmi V <lakshmivs.tech@gmail.com>
2026-09-18 23:56:05 +0530

    Add files via upload
    Square of a number or two.

---
+++ square.java
@@ -0,0 +1,9 @@
+class square{
+    public static void main(String args[])
+    {
+        int a=12, b=2, squarea, squareb;
+        squarea = a*a;
+        squareb = b*b;
+        System.out.println("Square of a is:\n"+squarea+"Square of b is:\n"+squareb);
+    }
+}
